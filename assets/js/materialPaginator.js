70cfd51aa930e9ffb7239aacc2a62e303df37063
lazaro <lazaro@localhost.localdomain>
2015-06-14 23:42:19 -0300

    Continuando plugin de paginação

--- assets/js/materialPaginator.js
+++ assets/js/materialPaginator.js
@@ -14,10 +14,12 @@
             paginationClass: 'pagination',
             paginationContentClass: 'waves-effect',
             selectedClass: 'active',
-            prevClass: 'mdi-navigation-chevron-left',
-            nextClass: 'mdi-navigation-chevron-right',
+            prevClass: 'prev',
+            nextClass: 'next',
+            prevIcon: 'mdi-navigation-chevron-left',
+            nextIcon: 'mdi-navigation-chevron-right',
             numberClass: 'number',
-            totalElements: 0,
+            totalElements: 0
         };
 
         var plugin = this;
@@ -33,26 +35,30 @@
 
         function buildPaginator() {
             plugin.paginatorContainer = $('<ul></ul>').addClass(plugin.settings.paginationClass);
-            plugin.paginatorContainer.append(buildPaginatorContent('prev', null));
+            plugin.paginatorContainer.append(buildPaginatorContent(plugin.settings.prevClass, null));
             for (var i = 1; i <= calcPages(); i++) {
-                plugin.paginatorContainer.append(buildPaginatorContent('number', i));
+                plugin.paginatorContainer.append(buildPaginatorContent(plugin.settings.numberClass, i));
             }
-            
-            plugin.paginatorContainer.append(buildPaginatorContent('next', null));
+
+            plugin.paginatorContainer.append(buildPaginatorContent(plugin.settings.nextClass, null));
             plugin.paginatorContainer.children('li').first().addClass('disabled');
+            plugin.paginatorContainer.children('li.' + plugin.settings.numberClass).first().addClass(plugin.settings.selectedClass);
             plugin.after(plugin.paginatorContainer);
-        };
+        }
+        ;
 
         function buildPaginatorContent(type, dataPos) {
             var pContent = $('<li></li>').addClass(plugin.settings.paginationContentClass);
             switch (type) {
-                case 'prev':
-                    pContent.append('<a href="#!"><i class="' + plugin.settings.prevClass + '"></a></li>');
+                case plugin.settings.prevClass:
+                    pContent.addClass(plugin.settings.prevClass).
+                            append('<a href="#!"><i class="' + plugin.settings.prevIcon + '"></a></li>');
                     break;
-                case 'next':
-                    pContent.append('<a href="#!"><i class="' + plugin.settings.nextClass + '"></a></li>');
+                case plugin.settings.nextClass:
+                    pContent.addClass(plugin.settings.nextClass).
+                            append('<a href="#!"><i class="' + plugin.settings.nextIcon + '"></a></li>');
                     break;
-                case 'number':
+                case plugin.settings.numberClass:
                     pContent.addClass(plugin.settings.numberClass)
                             .append('<a href="#!" data-pos="' + dataPos + '">' + dataPos + '</a></li>');
                     break;
@@ -64,10 +70,58 @@
         function clickEvent() {
             plugin.paginatorContainer.find('li').each(function () {
                 $(this).click(function () {
-                    if ($(this).hasClass('number')) {
+                    var active = plugin.paginatorContainer.find('li.' + plugin.settings.selectedClass);
+                    
+                    if ($(this).hasClass(plugin.settings.selectedClass) || $(this).hasClass('.disabled')) {
+                        $(this).click(function (e) {
+                            e.preventDefault();
+                        });
+                        return;
+                    }
+                    if ($(this).hasClass(plugin.settings.numberClass)) {
                         plugin.paginatorContainer.find('li').removeClass(plugin.settings.selectedClass);
                         $(this).addClass(plugin.settings.selectedClass);
+
+                        if ($(this).prev().hasClass(plugin.settings.prevClass)) {
+                            $(this).prev().addClass('disabled');
+                        } else if (!$(this).prev().hasClass(plugin.settings.prevClass)) {
+                            plugin.paginatorContainer.children('.' + plugin.settings.prevClass).removeClass('disabled');
+                        };
+
+                        if ($(this).next().hasClass(plugin.settings.nextClass)) {
+                            $(this).next().addClass('disabled');
+                        } else if (!$(this).next().hasClass(plugin.settings.nextClass)) {
+                            plugin.paginatorContainer.children('.' + plugin.settings.nextClass).removeClass('disabled');
+                        };
                     };
+                    if ($(this).hasClass(plugin.settings.prevClass)) {
+                        if (active.prev().is('.'+plugin.settings.prevClass)) {
+                            $(this).addClass('disabled');
+                        } else {
+                            plugin.paginatorContainer.children('.'+plugin.settings.nextClass).removeClass('disabled');
+                            active.removeClass(plugin.settings.selectedClass);
+                            active.prev('.' + plugin.settings.numberClass).addClass(plugin.settings.selectedClass);
+                            active = active.prev();
+                        };
+                        
+                    };
+                    if ($(this).hasClass(plugin.settings.nextClass)) {                        
+                        if (active.next().is('.'+plugin.settings.nextClass)) {
+                            $(this).addClass('disabled');
+                        } else {
+                            plugin.paginatorContainer.children('.'+plugin.settings.prevClass).removeClass('disabled');
+                            active.removeClass(plugin.settings.selectedClass);
+                            active.next('.' + plugin.settings.numberClass).addClass(plugin.settings.selectedClass);
+                            active = active.next();
+                        };
+
+                    };
+                    if(active.next().hasClass(plugin.settings.nextClass)){
+                        active.next().addClass('disabled');
+                    }else if(active.prev().hasClass(plugin.settings.prevClass)){
+                        active.prev().addClass('disabled');
+                    };
+                    
                 });
             });
         }
